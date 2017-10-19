6060104245e545a02c91ab88578cbd2e31c05e00
Laci Szucs <ifrozen@gmail.com>
2017-10-19 23:52:35 +0200

    feat(di): naive service implementation

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 module.exports = function getDI({}={}) {
+  const modules = {};
+  const implementationNames = {}
   return Object.freeze({
-
+    get(name) {
+      return modules[name]();
+    },
+    registerModule(name, dependencies, implementations) {
+      if (modules[name])
+        throw new Error(`Already registered ${name} module.`);
+      modules[name] = () =>
+        implementations[implementationNames[name]](...dependencies);
+    },
+    setImplementation(name, implementationName) {
+      if (implementationNames[name])
+        throw new Error(`Implementation of ${name} is already set.`);
+      implementationNames[name] = implementationName;
+    }
   });
 };
--- index.spec.js
+++ index.spec.js
@@ -6,4 +6,17 @@ describe('DI', () => {
     const di = getDI();
     expect(di).to.be.an('object');
   });
+
+  describe('#registerModule', () => {
+    it('should register a module', () => {
+      const di = getDI();
+      di.registerModule('adder', [], {
+        binary: () => (a, b) => a + b
+      });
+      di.setImplementation('adder', 'binary')
+      const adderService = di.get('adder');
+      expect(adderService).to.be.a('function');
+      expect(adderService(5,7)).to.equal(12);
+    });
+  })
 })
